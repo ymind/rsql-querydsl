92b861d3f6408da16ce4dd5c7ad17ef36799bfe0
ymind <ymind@yi.team>
2020-09-07 12:47:48 +0800

    chore(deps): bumped spring version from 2.3.2.RELEASE to 2.3.3.RELEASE

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,10 +34,10 @@ repositories {
 
 dependencies {
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-test
-    testImplementation("org.springframework.boot:spring-boot-starter-test:2.3.2.RELEASE") {
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.3.3.RELEASE") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
-    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa:2.3.2.RELEASE")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa:2.3.3.RELEASE")
 
     // https://mvnrepository.com/artifact/com.h2database/h2
     testImplementation("com.h2database:h2:1.4.200")
