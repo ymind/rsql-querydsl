e59fcc40afe374a7b368fbe8c2f706fd30581016
ymind <ymind@yi.team>
2020-09-07 13:06:11 +0800

    perf(util): enhance DateUtil

--- src/main/kotlin/team/yi/rsql/querydsl/util/DateUtil.kt
+++ src/main/kotlin/team/yi/rsql/querydsl/util/DateUtil.kt
@@ -1,65 +1,77 @@
 package team.yi.rsql.querydsl.util
 
-import java.text.ParseException
 import java.text.SimpleDateFormat
+import java.time.Instant
 import java.util.*
 
+@Suppress("MemberVisibilityCanBePrivate")
 object DateUtil {
-    private val DATE_FORMAT_REGEXPS: MutableMap<String, String> = HashMap()
+    val regexOptions: Set<RegexOption> = setOf(RegexOption.IGNORE_CASE)
 
-    @Throws(ParseException::class)
+    val DATE_FORMATS: Map<String, Regex> = mapOf(
+        "dd MMMM yyyy HH:mm:ss" to Regex("^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}\\s\\d{1,2}:\\d{1,2}:\\d{1,2}$", regexOptions),
+        "dd MMM yyyy HH:mm:ss" to Regex("^\\d{1,2}\\s[a-z]{3}\\s\\d{4}\\s\\d{1,2}:\\d{1,2}:\\d{1,2}$", regexOptions),
+        "yyyy/MM/dd HH:mm:ss" to Regex("^\\d{4}/\\d{1,2}/\\d{1,2}\\s\\d{1,2}:\\d{1,2}:\\d{1,2}$", regexOptions),
+        "MM/dd/yyyy HH:mm:ss" to Regex("^\\d{1,2}/\\d{1,2}/\\d{4}\\s\\d{1,2}:\\d{1,2}:\\d{1,2}$", regexOptions),
+        "yyyy-MM-dd'T'HH:mm:ss" to Regex("^\\d{4}-\\d{1,2}-\\d{1,2}'?T'?\\d{1,2}:\\d{1,2}:\\d{1,2}$", regexOptions),
+        "yyyy-MM-dd'T'HH:mm:ss'Z'" to Regex("^\\d{4}-\\d{1,2}-\\d{1,2}'?T'?\\d{1,2}:\\d{1,2}:\\d{1,2}'?Z'?$", regexOptions),
+        "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'" to Regex("^\\d{4}-\\d{1,2}-\\d{1,2}'?T'?\\d{1,2}:\\d{1,2}:\\d{1,2}\\.\\d{3}'?Z'?$", regexOptions),
+        "yyyy-MM-dd HH:mm:ss" to Regex("^\\d{4}-\\d{1,2}-\\d{1,2}\\s\\d{1,2}:\\d{1,2}:\\d{1,2}$", regexOptions),
+        "dd-MM-yyyy HH:mm:ss" to Regex("^\\d{1,2}-\\d{1,2}-\\d{4}\\s\\d{1,2}:\\d{1,2}:\\d{1,2}$", regexOptions),
+        "yyyyMMdd HHmmss" to Regex("^\\d{8}\\s\\d{6}$", regexOptions),
+        "dd MMMM yyyy HH:mm" to Regex("^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}\\s\\d{1,2}:\\d{1,2}$", regexOptions),
+        "dd MMM yyyy HH:mm" to Regex("^\\d{1,2}\\s[a-z]{3}\\s\\d{4}\\s\\d{1,2}:\\d{1,2}$", regexOptions),
+        "yyyy/MM/dd HH:mm" to Regex("^\\d{4}/\\d{1,2}/\\d{1,2}\\s\\d{1,2}:\\d{1,2}$", regexOptions),
+        "MM/dd/yyyy HH:mm" to Regex("^\\d{1,2}/\\d{1,2}/\\d{4}\\s\\d{1,2}:\\d{1,2}$", regexOptions),
+        "yyyy-MM-dd HH:mm" to Regex("^\\d{4}-\\d{1,2}-\\d{1,2}\\s\\d{1,2}:\\d{1,2}$", regexOptions),
+        "yyyy.MM.dd HH:mm" to Regex("^\\d{4}.\\d{1,2}.\\d{1,2}\\s\\d{1,2}:\\d{1,2}$", regexOptions),
+        "dd-MM-yyyy HH:mm" to Regex("^\\d{1,2}-\\d{1,2}-\\d{4}\\s\\d{1,2}:\\d{1,2}$", regexOptions),
+        "yyyyMMdd HHmm" to Regex("^\\d{8}\\s\\d{4}$", regexOptions),
+        "dd MMMM yyyy" to Regex("^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}$", regexOptions),
+        "dd MMM yyyy" to Regex("^\\d{1,2}\\s[a-z]{3}\\s\\d{4}$", regexOptions),
+        "yyyy/MM/dd" to Regex("^\\d{4}/\\d{1,2}/\\d{1,2}$", regexOptions),
+        "MM/dd/yyyy" to Regex("^\\d{1,2}/\\d{1,2}/\\d{4}$", regexOptions),
+        "yyyy.MM.dd" to Regex("^\\d{4}\\.\\d{1,2}\\.\\d{1,2}$", regexOptions),
+        "yyyy-MM-dd" to Regex("^\\d{4}-\\d{1,2}-\\d{1,2}$", regexOptions),
+        "dd.MM.yyyy" to Regex("^\\d{1,2}\\.\\d{1,2}\\.\\d{4}$", regexOptions),
+        "dd-MM-yyyy" to Regex("^\\d{1,2}-\\d{1,2}-\\d{4}$", regexOptions),
+        "yyyy" to Regex("^\\d{4}$", regexOptions),
+        "yy-MM-dd HH:mm" to Regex("^\\d{2}-\\d{1,2}-\\d{1,2}\\s\\d{1,2}:\\d{1,2}$", regexOptions),
+        "yy.MM.dd" to Regex("^\\d{2}\\.\\d{1,2}\\.\\d{1,2}$", regexOptions),
+        "yy-MM-dd" to Regex("^\\d{2}-\\d{1,2}-\\d{1,2}$", regexOptions),
+        "yy-MM" to Regex("^\\d{2}-\\d{1,2}\$", regexOptions),
+        "yyyy.MM" to Regex("^\\d{4}\\.\\d{1,2}\$", regexOptions),
+        "yyyy-MM" to Regex("^\\d{4}-\\d{1,2}\$", regexOptions),
+        "MM.yyyy" to Regex("^\\d{1,2}\\.\\d{4}$", regexOptions),
+        "MM-yyyy" to Regex("^\\d{1,2}-\\d{4}$", regexOptions),
+    )
+
     fun parse(dateString: String): Date? {
         return parse(dateString, determineDateFormat(dateString) ?: return null)
     }
 
-    @Throws(ParseException::class)
     fun parse(dateString: String, dateFormat: String): Date? {
-        val format = if (dateFormat.isBlank()) determineDateFormat(dateString) else dateFormat
-        val simpleDateFormat = SimpleDateFormat(format ?: return null)
+        val format = if (dateFormat.isBlank()) return null else dateFormat
 
-        simpleDateFormat.isLenient = false
+        return try {
+            val sdf = SimpleDateFormat(format)
+            sdf.isLenient = false
 
-        return simpleDateFormat.parse(dateString.trim('\'', '"'))
+            sdf.parse(dateString.replace("'?", "'"))
+        } catch (e: Exception) {
+            try {
+                Date.from(Instant.parse(dateString))
+            } catch (ex: Exception) {
+                null
+            }
+        }
     }
 
     fun determineDateFormat(dateString: String): String? {
-        return DATE_FORMAT_REGEXPS.keys
-            .firstOrNull { dateString.toLowerCase().matches(it.toRegex()) }
-            ?.let { DATE_FORMAT_REGEXPS[it] }
-    }
+        for ((t, u) in DATE_FORMATS) {
+            if (u.matches(dateString)) return t
+        }
 
-    init {
-        DATE_FORMAT_REGEXPS["^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$"] = "dd MMMM yyyy HH:mm:ss"
-        DATE_FORMAT_REGEXPS["^\\d{1,2}\\s[a-z]{3}\\s\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$"] = "dd MMM yyyy HH:mm:ss"
-        DATE_FORMAT_REGEXPS["^\\d{4}/\\d{1,2}/\\d{1,2}\\s\\d{1,2}:\\d{2}:\\d{2}$"] = "yyyy/MM/dd HH:mm:ss"
-        DATE_FORMAT_REGEXPS["^\\d{1,2}/\\d{1,2}/\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$"] = "MM/dd/yyyy HH:mm:ss"
-        DATE_FORMAT_REGEXPS["^\\d{4}-\\d{1,2}-\\d{1,2}\\s\\d{1,2}:\\d{2}:\\d{2}$"] = "yyyy-MM-dd HH:mm:ss"
-        DATE_FORMAT_REGEXPS["^\\d{1,2}-\\d{1,2}-\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$"] = "dd-MM-yyyy HH:mm:ss"
-        DATE_FORMAT_REGEXPS["^\\d{8}\\s\\d{6}$"] = "yyyyMMdd HHmmss"
-        DATE_FORMAT_REGEXPS["^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}\\s\\d{1,2}:\\d{2}$"] = "dd MMMM yyyy HH:mm"
-        DATE_FORMAT_REGEXPS["^\\d{1,2}\\s[a-z]{3}\\s\\d{4}\\s\\d{1,2}:\\d{2}$"] = "dd MMM yyyy HH:mm"
-        DATE_FORMAT_REGEXPS["^\\d{4}/\\d{1,2}/\\d{1,2}\\s\\d{1,2}:\\d{2}$"] = "yyyy/MM/dd HH:mm"
-        DATE_FORMAT_REGEXPS["^\\d{1,2}/\\d{1,2}/\\d{4}\\s\\d{1,2}:\\d{2}$"] = "MM/dd/yyyy HH:mm"
-        DATE_FORMAT_REGEXPS["^\\d{4}-\\d{1,2}-\\d{1,2}\\s\\d{1,2}:\\d{2}$"] = "yyyy-MM-dd HH:mm"
-        DATE_FORMAT_REGEXPS["^\\d{4}.\\d{1,2}.\\d{1,2}\\s\\d{1,2}:\\d{2}$"] = "yyyy-MM-dd HH:mm"
-        DATE_FORMAT_REGEXPS["^\\d{1,2}-\\d{1,2}-\\d{4}\\s\\d{1,2}:\\d{2}$"] = "dd-MM-yyyy HH:mm"
-        DATE_FORMAT_REGEXPS["^\\d{8}\\s\\d{4}$"] = "yyyyMMdd HHmm"
-        DATE_FORMAT_REGEXPS["^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}$"] = "dd MMMM yyyy"
-        DATE_FORMAT_REGEXPS["^\\d{1,2}\\s[a-z]{3}\\s\\d{4}$"] = "dd MMM yyyy"
-        DATE_FORMAT_REGEXPS["^\\d{4}/\\d{1,2}/\\d{1,2}$"] = "yyyy/MM/dd"
-        DATE_FORMAT_REGEXPS["^\\d{1,2}/\\d{1,2}/\\d{4}$"] = "MM/dd/yyyy"
-        DATE_FORMAT_REGEXPS["^\\d{4}.\\d{1,2}.\\d{1,2}$"] = "yyyy.MM.dd"
-        DATE_FORMAT_REGEXPS["^\\d{4}-\\d{1,2}-\\d{1,2}$"] = "yyyy-MM-dd"
-        DATE_FORMAT_REGEXPS["^\\d{1,2}.\\d{1,2}.\\d{4}$"] = "dd.MM.yyyy"
-        DATE_FORMAT_REGEXPS["^\\d{1,2}-\\d{1,2}-\\d{4}$"] = "dd-MM-yyyy"
-        DATE_FORMAT_REGEXPS["^\\d{4}$"] = "yyyy"
-        DATE_FORMAT_REGEXPS["^\\d{2}$-\\d{1,2}-\\d{1,2}\\s\\d{1,2}:\\d{2}$"] = "yy-MM-dd HH:mm"
-        DATE_FORMAT_REGEXPS["^\\d{2}$.\\d{1,2}.\\d{1,2}$"] = "yy.MM.dd"
-        DATE_FORMAT_REGEXPS["^\\d{2}$-\\d{1,2}-\\d{1,2}$"] = "yy-MM-dd"
-        DATE_FORMAT_REGEXPS["^\\d{2}$-\\d{1,2}"] = "yy-MM"
-        DATE_FORMAT_REGEXPS["^\\d{4}$.\\d{1,2}"] = "yyyy-MM"
-        DATE_FORMAT_REGEXPS["^\\d{4}$-\\d{1,2}"] = "yyyy-MM"
-        DATE_FORMAT_REGEXPS["^\\d{1,2}.\\d{4}$"] = "MM-yyyy"
-        DATE_FORMAT_REGEXPS["^\\d{1,2}-\\d{4}$"] = "MM-yyyy"
+        return null
     }
 }
---
+++ src/test/kotlin/team/yi/rsql/querydsl/test/kotlintest/util/DateUtilTests.kt
@@ -0,0 +1,71 @@
+package team.yi.rsql.querydsl.test.kotlintest.util
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+import team.yi.rsql.querydsl.util.DateUtil
+
+class DateUtilTests {
+    private val log = LoggerFactory.getLogger(DateUtilTests::class.java)
+
+    private val data = mapOf(
+        "dd MMMM yyyy HH:mm:ss" to "19 july 2022 13:7:24",
+        "dd MMM yyyy HH:mm:ss" to "19 jul 2022 13:47:24",
+        "yyyy/MM/dd HH:mm:ss" to "2022/07/19 13:47:24",
+        "MM/dd/yyyy HH:mm:ss" to "07/19/2022 13:47:24",
+        "yyyy-MM-dd'?T'?HH:mm:ss" to "2022-07-19T13:47:24",
+        "yyyy-MM-dd'?T'?HH:mm:ss'?Z'?" to "2022-07-19T13:47:24Z",
+        "yyyy-MM-dd'?T'?HH:mm:ss.SSS'?Z'?" to "2022-07-19T13:47:24.345Z",
+        "yyyy-MM-dd HH:mm:ss" to "2022-07-19 13:47:24",
+        "dd-MM-yyyy HH:mm:ss" to "19-07-2022 13:47:24",
+        "yyyyMMdd HHmmss" to "20220719 134724",
+        "dd MMMM yyyy HH:mm" to "19 july 2022 13:47",
+        "dd MMM yyyy HH:mm" to "19 jul 2022 13:47",
+        "yyyy/MM/dd HH:mm" to "2022/07/19 13:47",
+        "MM/dd/yyyy HH:mm" to "07/19/2022 13:47",
+        "yyyy-MM-dd HH:mm" to "2022-07-19 13:47",
+        "yyyy.MM.dd HH:mm" to "2022.07.19 13:47",
+        "dd-MM-yyyy HH:mm" to "19-07-2022 13:47",
+        "yyyyMMdd HHmm" to "20220719 1347",
+        "dd MMMM yyyy" to "19 july 2022",
+        "dd MMM yyyy" to "19 jul 2022",
+        "yyyy/MM/dd" to "2022/07/19",
+        "MM/dd/yyyy" to "07/19/2022",
+        "yyyy.MM.dd" to "2022.07.19",
+        "yyyy-MM-dd" to "2022-07-19",
+        "dd.MM.yyyy" to "19.07.2022",
+        "dd-MM-yyyy" to "19-07-2022",
+        "yyyy" to "2022",
+        "yy-MM-dd HH:mm" to "22-07-19 13:47",
+        "yy.MM.dd" to "22.07.19",
+        "yy-MM-dd" to "22-07-19",
+        "yy-MM" to "22-07",
+        "yyyy.MM" to "2022.07",
+        "yyyy-MM" to "2022-07",
+        "MM.yyyy" to "07.2022",
+        "MM-yyyy" to "07-2022",
+    )
+
+    @Test
+    fun testDetermineDateFormat() {
+        data.forEach { (format, value) ->
+            val dateFormat = DateUtil.determineDateFormat(value)
+
+            log.info("{} - {}", format, dateFormat)
+
+            assertEquals(format.replace("'?", "'"), dateFormat)
+        }
+    }
+
+    @Test
+    fun testParse() {
+        data.forEach { (format, value) ->
+            val date = DateUtil.parse(value)
+
+            log.info("{} - {}", format, date)
+
+            assertNotNull(date)
+        }
+    }
+}
