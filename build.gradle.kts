7139ac4a477f35200a5910c770f2f2d988f49dc2
ymind <ymind@yi.team>
2021-10-27 21:02:50 +0800

    chore(deps): bumped spring version to 2.5.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,10 +39,10 @@ repositories {
 
 dependencies {
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-test
-    testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.2") {
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.6") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
-    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa:2.5.2")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa:2.5.6")
 
     // https://mvnrepository.com/artifact/com.h2database/h2
     testImplementation("com.h2database:h2:1.4.200")
