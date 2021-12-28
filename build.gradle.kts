d04b2337f617e02b31f85cccf49e2216309b2712
ymind <ymind@yi.team>
2021-12-28 09:15:30 +0800

    build(gradle/plugin): bumped se.patrikerdes.use-latest-versions version to 0.2.18

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
     id("team.yi.semantic-gitlog") version "0.5.17"
 
     // https://plugins.gradle.org/plugin/se.patrikerdes.use-latest-versions
-    id("se.patrikerdes.use-latest-versions") version "0.2.17"
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
     // https://plugins.gradle.org/plugin/com.github.ben-manes.versions
     id("com.github.ben-manes.versions") version "0.39.0"
 }
